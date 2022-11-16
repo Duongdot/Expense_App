58e5b448b2788e8d7f8f7602b50d9d23044aecbe
Duong <duong@FaceX.local>
2022-11-16 23:57:34 +0700

    done confirm trip

--- app/src/main/java/com/Duong/Expense/TripActivity/AddActivity.java
+++ app/src/main/java/com/Duong/Expense/TripActivity/AddActivity.java
@@ -1,10 +1,13 @@
 package com.Duong.Expense.TripActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -13,9 +16,11 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.Duong.Expense.Database.MyDatabaseHelper;
+import com.Duong.Expense.Object.Expense;
 import com.Duong.Expense.Object.Trip;
 import com.Duong.Expense.R;
 
@@ -27,11 +32,12 @@ import java.util.Locale;
 public class AddActivity extends AppCompatActivity {
 
     EditText Trip_input, Destination_input, DateFrom_input, DateTo_input, desc_input;
-    Button add_button;
+    Button add_button, add_button_popup, cancel_button_popup;
     Calendar calendar;
     RadioGroup radioGroup;
     RadioButton selectedRadioButton;
     String risk;
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,11 +77,12 @@ public class AddActivity extends AppCompatActivity {
         DatePickerDialog.OnDateSetListener datePickerTo = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                    calendar.set(Calendar.YEAR, year);
-                    calendar.set(Calendar.MONTH, month);
-                    calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                    updateCalendar();
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateCalendar();
             }
+
             private void updateCalendar() {
                 String format = "dd MMM yyyy";
                 SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.US);
@@ -108,21 +115,49 @@ public class AddActivity extends AppCompatActivity {
             showError(DateFrom_input);
         } else if (dateT.isEmpty()) {
             showError(DateTo_input);
-        }else if (new Date(dateF).after(new Date(dateT))){
+        } else if (new Date(dateF).after(new Date(dateT))) {
             showErrorDate(DateTo_input);
-        }
-        else if (Description.isEmpty()) {
+        } else if (Description.isEmpty()) {
             showError(desc_input);
         } else {
-            addTrip();
+            CheckConfirm();
         }
+    }
+
+    private void CheckConfirm() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        String tripName = Trip_input.getText().toString().trim();
+        String location = Destination_input.getText().toString().trim();
+        String dateF = DateFrom_input.getText().toString().trim();
+        String dateT = DateTo_input.getText().toString().trim();
+        String Description = desc_input.getText().toString().trim();
+        radioGroup = findViewById(R.id.radioGroup);
+        selectedRadioButton = findViewById(radioGroup.getCheckedRadioButtonId());
+        risk = selectedRadioButton.getText().toString();
+
+        builder.setTitle("Confirm Trip");
+        builder.setMessage("Do you want to add trip ?");
+        builder.setMessage("Trip name: " + tripName +
+                "\n Trip Destination: " + location + "\n Trip Date Start : " + dateF +
+                "\n Trip Date End: " + dateT + "\n Trip Risk : " + risk + "\n Trip Note : " + Description);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addTrip();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        builder.create().show();
     }
 
     private void showErrorDate(EditText dateTo_input) {
         dateTo_input.setError("Date invalid");
         dateTo_input.requestFocus();
     }
-
 
     private void addTrip() {
         MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
@@ -131,23 +166,22 @@ public class AddActivity extends AppCompatActivity {
         selectedRadioButton = findViewById(radioGroup.getCheckedRadioButtonId());
         risk = selectedRadioButton.getText().toString();
 
+        Trip trip = new Trip();
+        trip.setName(Trip_input.getText().toString().trim());
+        trip.setDesc(desc_input.getText().toString().trim());
+        trip.setDateFrom(DateFrom_input.getText().toString().trim());
+        trip.setDateTo(DateTo_input.getText().toString().trim());
+        trip.setDes(Destination_input.getText().toString().trim());
+        trip.setRisk(risk);
 
-            Trip trip = new Trip();
-            trip.setName(Trip_input.getText().toString().trim());
-            trip.setDesc(desc_input.getText().toString().trim());
-            trip.setDateFrom(DateFrom_input.getText().toString().trim());
-            trip.setDateTo(DateTo_input.getText().toString().trim());
-            trip.setDes(Destination_input.getText().toString().trim());
-            trip.setRisk(risk);
-
-            long result = myDB.addTrip(trip);
-            if (result == -1) {
-                Toast.makeText(getBaseContext(), "Failed", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getBaseContext(), "Added Successfully!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(AddActivity.this, TripActivity.class);
-                startActivity(intent);
-            }
+        long result = myDB.addTrip(trip);
+        if (result == -1) {
+            Toast.makeText(getBaseContext(), "Failed", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getBaseContext(), "Added Successfully!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(AddActivity.this, TripActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void showError(EditText input) {
